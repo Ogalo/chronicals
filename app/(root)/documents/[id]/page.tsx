9d085ce001f3b63bf043f36c53b6e50b69f51515
Ogalo <ogaloharrison@gmail.com>
2024-07-29 17:05:46 +0300

    Add button

---
+++ app/(root)/documents/[id]/page.tsx
@@ -0,0 +1,13 @@
+import Header from '@/components/Header'
+import { Editor } from '@/components/editor/Editor'
+import { SignInButton, SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
+
+const Document = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Document
---
+++ app/(root)/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Button } from "@/components/ui/button"
+
+const Home = () => {
+  return (
+    <div>
+      <Button>Finya me</Button>
+    </div>
+  )
+}
+
+export default Home
