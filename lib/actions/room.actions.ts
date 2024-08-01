c9a144baa6fd931bd8dbfa5f0a08f9023bb12770
Ogalo <ogaloharrison@gmail.com>
2024-08-01 06:20:08 +0300

    Enable live collaboration

--- lib/actions/room.actions.ts
+++ lib/actions/room.actions.ts
@@ -20,7 +20,7 @@ export const createDocument = async ({ userId, email }: CreateDocumentParams) =>
     const room = await liveblocks.createRoom(roomId, {
       metadata,
       usersAccesses,
-      defaultAccesses: []
+      defaultAccesses: ['room:write']
 
     });
 
@@ -33,4 +33,24 @@ export const createDocument = async ({ userId, email }: CreateDocumentParams) =>
     console.log(`Error happened while creating a room: ${error}`);
 
   }
+}
+
+export const getDocument = async ({ roomId, userId } : {roomId: string; userId: string }) => {
+
+ try {
+  const room = await liveblocks.getRoom(roomId);
+
+  //TODO: Bring this back on 
+
+  // const hasAccess = Object.keys(room.usersAccesses).includes(userId);
+
+  // if(!hasAccess){
+  //   throw new Error('You do not have access to this meditation');
+  // }
+
+  return parseStringify(room);
+} catch (error){
+  console.log(`Error happened while getting the meditation ${error}`)
+}
+
 }
