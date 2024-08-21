054fdffda0faa9b71d658ad8aa279b4d6d018d6e
Ogalo <ogaloharrison@gmail.com>
2024-08-21 07:52:00 +0300

    Add sentry for diagnostics

---
+++ sentry.server.config.ts
@@ -0,0 +1,19 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://97f72463991cffb4ccfe1d285c23c537@o4507053856063488.ingest.us.sentry.io/4507772885598208",
+
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+
+  // Uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: process.env.NODE_ENV === 'development',
+  
+});
